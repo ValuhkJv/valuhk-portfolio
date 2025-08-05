fe902728518ab3772b3d20cdd63fd5a48c3a181e
ValuhkJv <valuhkjv@gmail.com>
2025-08-05 11:33:58 +0700

    deploy - portofolio Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+
+  // Penting: Sesuaikan dengan nama repository Anda
+  basePath: process.env.NODE_ENV === "production" ? "/valuhk-portfolio" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/valuhk-portfolio/" : "",
+
+  // Nonaktifkan optimisasi gambar bawaan Next.js karena tidak didukung di GitHub Pages
   images: {
     unoptimized: true,
   },
